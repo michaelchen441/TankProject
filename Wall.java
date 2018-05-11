64de0bb85b527f35552cb83729b5c901ed22bf50
erramineni <39103328+erramineni@users.noreply.github.com>
2018-05-11 13:28:22 -0700

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,11 +1,36 @@
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Wall
 {
 	private boolean destructable;
+	private int xLoc;
+	private int yLoc;
+	private int length;
+	private int width;
 	
-	
-	void draw()
-	{
+	public Wall(int x, int y,int l,int w) {
+		xLoc = x;
+		yLoc = y;
+		length = l;
+		width = w;
 		
+	}
+	public int getXLoc() {
+		return xLoc;
+	}
+	public int getYLoc() {
+		return yLoc;
+	}
+	public int getLength() {
+		return length;
+	}
+	public int getWidth() {
+		return width;
+	}
+	void draw(Graphics g){
+		g.setColor(Color.BLACK);
+		g.drawRect(xLoc, yLoc, length, width);
+		g.fillRect(xLoc, yLoc, length, width);
 	}
 }
