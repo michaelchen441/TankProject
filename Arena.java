ee8365443520fc0b3d04946246f98e02f378bc90
erramineni <39103328+erramineni@users.noreply.github.com>
2018-05-11 13:23:10 -0700

    Update Arena.java

--- Arena.java
+++ Arena.java
@@ -1,8 +1,18 @@
+import java.awt.Graphics;
 import java.util.ArrayList;
 
 public class Arena
 {
 	private ArrayList<Wall> walls;
-	
-	
+	public Arena(ArrayList<Wall> w) {
+		walls = w;
+	}
+	public ArrayList<Wall> getWalls(){
+		return walls;
+	}
+	public void draw(Graphics g) {
+		for(Wall w : walls) {
+			w.draw(g);
+		}
+	}
 }
