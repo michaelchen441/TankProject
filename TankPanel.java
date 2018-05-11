d806268c80fa3f2089565bca9782913521bd28ca
erramineni <39103328+erramineni@users.noreply.github.com>
2018-05-11 13:26:20 -0700

    Update TankPanel.java

--- TankPanel.java
+++ TankPanel.java
@@ -2,6 +2,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
@@ -12,7 +13,7 @@ import javax.swing.UIManager;
 
 public class TankPanel extends JPanel {
 
-
+	PlayerTank t = new PlayerTank(20,20);
 	boolean showMenu = true;
 	
 	Timer timer = new Timer(500,null);
@@ -45,8 +46,8 @@ public class TankPanel extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				// what do you want to do when the left arrow is pressed?
 				System.out.println("Hit left arrow!!");
-				
-				
+				t.move(-7,0);
+				repaint();
 			}
 		});
 		
@@ -58,7 +59,8 @@ public class TankPanel extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				// what do you want to do when the left arrow is pressed?
 				System.out.println("Hit right arrow!!");
-				
+				t.move(7,0);
+				repaint();
 			}
 		});
 		
@@ -69,7 +71,8 @@ public class TankPanel extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				// what do you want to do when the left arrow is pressed?
 				System.out.println("Hit up arrow!!");
-			
+				t.move(0,7);
+				repaint();
 			}
 		});
 		
@@ -80,7 +83,8 @@ public class TankPanel extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				// what do you want to do when the left arrow is pressed?
 				System.out.println("Hit down arrow!!");
-		
+				t.move(0,-7);
+				repaint();
 			}
 		});
 		this.requestFocusInWindow();
@@ -100,11 +104,38 @@ public class TankPanel extends JPanel {
 	}
 	protected void tick() {
 		System.out.println("Timer went off!");
-		//repaint();
+		repaint();
 	}
 	public void paintComponent(Graphics g) {
-		PlayerTank t = new PlayerTank(20,20);
 		t.draw(g);
+	//Level One	
+		ArrayList<Wall> levelOne = new ArrayList<Wall>();
+		Arena a = new Arena(levelOne);
+		Wall w1 = new Wall(90,80,10,200);
+		Wall w2 = new Wall(90,80,200,10);
+		
+		Wall w3 = new Wall(90,480,10,200);
+		Wall w4 = new Wall(90,680,200,10);
+		
+		Wall w5 = new Wall(520,80,200,10);
+		Wall w6 = new Wall(720,80,10,200);
+		
+		Wall w7 = new Wall(520,680,200,10);
+		Wall w8 = new Wall(720,480,10,210);
+		
+		
+		levelOne.add(w1);
+		levelOne.add(w2);
+		levelOne.add(w3);
+		levelOne.add(w4);
+		levelOne.add(w5);
+		levelOne.add(w6);
+		levelOne.add(w7);
+		levelOne.add(w8);
+		a.draw(g);
+		
+	//Level
+		
 		
 	}
 
