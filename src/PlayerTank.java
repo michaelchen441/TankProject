0b041c087755f93b545792d68765a01a901e1796
Daniel <daniellovelock22@Gmail.com>
2018-05-16 14:17:09 -0700

    adds stockpile and turretAngle to playertank started projectile class

--- src/PlayerTank.java
+++ src/PlayerTank.java
@@ -1,17 +1,19 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 public class PlayerTank extends Tank
 {
-	
+	ArrayList<Projectile> stockPile = new ArrayList<Projectile>();
+	double turretAngle;
 	
 	
 	public PlayerTank(int inX, int inY)
 	{
 		super();
 		type = TankType.GREEN;
-		xLoc = inX;
-		yLoc = inY;
+		xLoc = inX * 50; //miltiples so they can be set up with the same grid as walls
+		yLoc = inY * 50;
 		
 	}
 	
@@ -27,16 +29,16 @@ public class PlayerTank extends Tank
 	
 	public void fire()
 	{
-		
+		if(stockPile.size() > 0) {
+			new Projectile(xLoc,yLoc,TankType.GREEN, turretAngle);
+			stockPile.remove(stockPile.size());
+		}
 		
 	}
 	
 	void draw(Graphics g)
 	{
-		g.setColor(Color.BLACK);
-		g.drawRect(xLoc, yLoc, width, height);
-		g.setColor(Color.GREEN);
-		g.fillRect(xLoc, yLoc, width, height);
+		
 		
 	}
 }
