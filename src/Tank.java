fd36e5a88e2cb1d98b54a4145779b26bdbe1d204
Daniel <daniellovelock22@Gmail.com>
2018-05-18 13:08:29 -0700

    good shit

--- src/Tank.java
+++ src/Tank.java
@@ -21,62 +21,67 @@ public abstract class Tank
 	public boolean canMove(Direction dir, Wall[][] walls) {
 		if(dir == null)
 			return true;
-
 		if(dir == Direction.NORTH) 
-			for(int r = 0; r<walls.length; r++) 
-				for(int c = 0; c<walls[r].length; c++) 
-					if(walls[r][c] != null) 
-						
-						
-						if(yLoc == (r*50)+50) {
-							
-							if(xLoc > (c*50)-50 && xLoc < (c*50)+50){
-								return false;
-							}
-						}
-							
-							
-//						if(yLoc == (r*50)+50) 
-//							for(int i = 0;i<50; i++) 
-//								for(int b = 0;b<50; b++) 
-//									if(xLoc+i == (c*50)+b)
-//										return false;
-
-		if(dir == Direction.EAST) {
-			for(int r = 0; r<walls.length; r++) 
-				for(int c = 0; c<walls[r].length; c++) 
-					if(walls[r][c] != null) 
-						if(xLoc+50 == c*50) 
-							for(int i = 0;i<50; i++) 
-								for(int b = 0;b<50; b++) 
-									if(yLoc+i == (r*50)+b)
-										return false;
-
-		}
-		if(dir == Direction.SOUTH) {
-			for(int r = 0; r<walls.length; r++) 
-				for(int c = 0; c<walls[r].length; c++) 
-					if(walls[r][c] != null) 
-						if(yLoc+50 == r*50) 
-							for(int i = 0;i<50; i++) 
-								for(int b = 0;b<50; b++) 
-									if(xLoc+i == (c*50)+b)
-										return false;
-		}
-		if(dir == Direction.WEST) {
-			for(int r = 0; r<walls.length; r++) 
-				for(int c = 0; c<walls[r].length; c++) 
-					if(walls[r][c] != null) 
-						if(xLoc == (c*50)+50) 
-							for(int i = 0;i<50; i++) 
-								for(int b = 0;b<50; b++) 
-									if(yLoc+i == (r*50)+b)
-										return false;
-		}
-
+			return checkNorth(walls);
+		if(dir == Direction.EAST) 
+			return checkEast(walls);
+		if(dir == Direction.SOUTH) 
+			return checkSouth(walls);
+		if(dir == Direction.WEST) 
+			return checkWest(walls);
+		if(dir == Direction.NORTHWEST) 
+			return checkNorth(walls) && checkWest(walls);
+		if(dir == Direction.NORTHEAST) 
+			return checkNorth(walls) && checkEast(walls);
+		if(dir == Direction.SOUTHWEST) 
+			return checkSouth(walls) && checkWest(walls);
+		if(dir == Direction.SOUTHEAST) 
+			return checkSouth(walls) && checkEast(walls);
 		return true;
 
 	}
+	private boolean checkWest(Wall[][] walls) {
+		for(int r = 0; r<walls.length; r++) 
+			for(int c = 0; c<walls[r].length; c++) 
+				if(walls[r][c] != null) 
+					if(xLoc == (c*50)+50) 
+						if(yLoc > r*50 && yLoc < (r*50)+50 || yLoc+50 >r*50 && yLoc+50 < (r*50)+50)
+							return false;
+		return true;
+	}
+
+	private boolean checkSouth(Wall[][] walls) {
+		for(int r = 0; r<walls.length; r++) 
+			for(int c = 0; c<walls[r].length; c++) 
+				if(walls[r][c] != null) 
+					if(yLoc+50 == r*50) 
+						if(xLoc > c*50 && xLoc < (c*50)+50 || xLoc+50 >c*50 && xLoc+50 < (c*50)+50)
+							return false;
+
+		return true;
+	}
+
+	private boolean checkEast(Wall[][] walls) {
+		for(int r = 0; r<walls.length; r++) 
+			for(int c = 0; c<walls[r].length; c++) 
+				if(walls[r][c] != null) 
+					if(xLoc+50 == c*50) 
+						if(yLoc > r*50 && yLoc < (r*50)+50 || yLoc+50 > r*50 && yLoc+50 < (r*50)+50)
+							return false;
+		return true;
+	}
+
+	private boolean checkNorth(Wall[][] walls) {
+		for(int r = 0; r<walls.length; r++) 
+			for(int c = 0; c<walls[r].length; c++) 
+				if(walls[r][c] != null) 
+					if(yLoc == (r*50)+50) 
+						if(xLoc > c*50 && xLoc < (c*50)+50 || xLoc+50 > c*50 && xLoc+50 < (c*50)+50)
+							return false;
+		return true;
+
+	}
+
 	abstract void move();
 	abstract void aim();
 	abstract void fire();
