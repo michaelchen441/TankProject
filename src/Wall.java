93aac8e98fa622d6949fe4a949e316ebec44c807
eswarramineni <eswarramineni@10.141.68.10>
2018-05-20 00:32:15 -0700

    Comments finalized for wall and menu

--- src/Wall.java
+++ src/Wall.java
@@ -2,12 +2,14 @@ import java.awt.Graphics;
 
 public class Wall
 {
-	private boolean destructable;
+	private boolean destructable; //Type of wall
+	//Wall x and y location
 	private int xLoc;
 	private int yLoc;
 
 
 	public Wall(int y, int x, boolean inDestructable) {
+		// Wall is 50 pixels wide and long
 		xLoc = x*50;
 		yLoc = y*50;
 		destructable = inDestructable;
@@ -16,17 +18,19 @@ public class Wall
 }
 
 	public int getXLoc() {
-		return xLoc;
+		return xLoc; //Returns x location
 	}
 	public int getYLoc() {
-		return yLoc;
+		return yLoc; //Returns y location
 	}
 
 
 	void draw(Graphics g, ImageLibrary l){
+		//Draws destructable wall
 		if(destructable){
 			g.drawImage(l.destructableWall, xLoc, yLoc, null);		
 		}
+		//Draws indestructable wall
 		else{
 			g.drawImage(l.indestructableWall, xLoc, yLoc, null);	
 		}
