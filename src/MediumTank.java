93f843567283f72db4e349d03d65134b0bb795ce
Wilmerlin01 <wilmerlin01@gmail.com>
2018-05-20 18:08:06 -0700

    MadeSomeTankyBois

---
+++ src/MediumTank.java
@@ -0,0 +1,31 @@
+import java.awt.Graphics;
+
+public class MediumTank extends AITank {
+
+	public MediumTank(TankType inType, int x, int y, Wall[][] walls) {
+		super(inType, x, y, walls);
+		// TODO Auto-generated constructor stub
+	}
+
+	void move()
+	{
+		
+	}
+	//Need to figure out mechanism by which AI Tank Aims
+	void aim()
+	{
+		
+	}
+	//Need to figure out mechanism by which AI Tank Fires
+	void fire()
+	{
+		//create projectile with input: type, 
+		
+	}
+	
+	void draw(Graphics g, ImageLibrary l)
+	{
+		
+	
+	}
+}
---
+++ src/SpedTank.java
@@ -0,0 +1,31 @@
+import java.awt.Graphics;
+
+public class SpedTank extends AITank {
+
+	public SpedTank(TankType inType, int x, int y, Wall[][] walls) {
+		super(inType, x, y, walls);
+		// TODO Auto-generated constructor stub
+	}
+	//Need to figure out mechanism by which AI Tank Moves
+		void move()
+		{
+			//this motherf*cker is a stationary bitch
+		}
+		//Need to figure out mechanism by which AI Tank Aims
+		void aim()
+		{
+			
+		}
+		//Need to figure out mechanism by which AI Tank Fires
+		void fire()
+		{
+			//create projectile with input: type, 
+			
+		}
+		
+		void draw(Graphics g, ImageLibrary l)
+		{
+			
+		
+		}
+}
